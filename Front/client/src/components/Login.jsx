1b8fe2a7a02f056c250b894eb576339e97a3bc03
DavainCI <rodrigodavalos2105@gmail.com>
2025-07-13 23:06:54 -0500

    delete backend local

--- Front/client/src/components/Login.jsx
+++ Front/client/src/components/Login.jsx
@@ -206,7 +206,7 @@ const Login = () => {
             disabled={isLoading}
           >
             <img 
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" 
+              src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" 
               alt="Google" 
             />
             {isLogin ? 'Iniciar con Google' : 'Registrarse con Google'}
