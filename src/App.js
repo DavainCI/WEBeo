f11252fa24d3a0ca255a8d6425bef8b713fa092e
DavainCI <rodrigodavalos2105@gmail.com>
2025-07-20 21:32:24 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Principal from './components/Principal';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
