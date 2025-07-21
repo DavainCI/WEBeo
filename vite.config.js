7b38755ee8ff07a967743a52ea1fc8f3cf327b40
DavainCI <rodrigodavalos2105@gmail.com>
2025-07-20 21:06:23 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/consumo-apis-gitpage",
+  base: "/WEBeo",
   plugins: [react()],
 });
