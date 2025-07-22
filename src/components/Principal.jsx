bd73f8d89c063b45156edca3e24306584377fe3d
DavainCI <rodrigodavalos2105@gmail.com>
2025-07-22 10:28:56 -0500

    a

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Principal.css';
-
 import starsGif from '../assets/stars.gif';
 import productoImg from '../assets/Tiburones.jpg';
 import Cv from './Cv';
